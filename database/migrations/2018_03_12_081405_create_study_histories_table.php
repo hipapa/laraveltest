c2cb560104ff7e515492b2c96b061df8b3feede4
Chigishitsu <chingishitsu_1219@yahoo.co.jp>
2018-03-12 17:50:42 +0900

    chenyishi 0312

---
+++ database/migrations/2018_03_12_081405_create_study_histories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudyHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('study_histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->date('start_at');
+            $table->date('end_at');
+            $table->string('event',255);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('study_histories');
+    }
+}
